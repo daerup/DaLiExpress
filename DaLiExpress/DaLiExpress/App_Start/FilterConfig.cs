8e57a7e374c492e7d8fbd6d3fe48381503675877
Liam <liam.gloggner@hotmail.ch>
2019-06-07 18:37:22 +0200

    Added VisualStudio Project with integrated DataBase

---
+++ DaLiExpress/DaLiExpress/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DaLiExpress
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
